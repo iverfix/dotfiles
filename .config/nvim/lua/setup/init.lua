61738a055a68e76b3547a16e7d0e9c73b8f26cdc
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2024-12-03 20:03:21 +0100

    MIgrated to lazy

--- .config/nvim/lua/setup/init.lua
+++ .config/nvim/lua/setup/init.lua
@@ -1,2 +1,5 @@
 require("setup.remap")
 require("setup.set")
+require("setup.lazy_init")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---
+++ .config/nvim/lua/setup/lazy/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                "jsdoc", "bash", "cpp"
+            },
+
+            sync_install = false,
+            auto_install = true,
+
+            indent = {
+                enable = true
+            },
+
+            highlight = {
+                enable = true,
+
+                additional_vim_regex_highlighting = { "markdown" },
+            },
+        })
+    end
+}
