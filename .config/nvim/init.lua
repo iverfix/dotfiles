455b4ae38122c92e7e5e02fb65306ca9aebfe67f
Iver <iver.andreas.ugelvik@hotmail.com>
2025-10-12 22:27:37 +0200

    Cleaned up some logic

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,5 @@
 require("config")
 
--- Disable netrw banner/header
-vim.g.netrw_banner = 0
-
--- Optional: cleaner netrw setup
-vim.g.netrw_liststyle = 3 -- tree-style view
-vim.g.netrw_browse_split = 4 -- open files in previous window
-vim.g.netrw_altv = 1 -- open splits to the right
-vim.g.netrw_winsize = 25 -- set netrw width
 vim.opt.clipboard:append("unnamedplus")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
