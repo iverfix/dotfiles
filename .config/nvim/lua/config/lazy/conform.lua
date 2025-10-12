378d1f9d7f93d0a8a1acd7bf93f3f7bd5c88e078
Iver <iver.andreas.ugelvik@hotmail.com>
2025-10-12 21:45:52 +0200

    Updated some config

--- .config/nvim/lua/config/lazy/conform.lua
+++ .config/nvim/lua/config/lazy/conform.lua
@@ -3,17 +3,16 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        cpp  =  {"clang_format_18" },
-        lua  = { "stylua" },
+        cpp = { "clang_format_18" },
+        lua = { "stylua" },
         -- add more filetypes if needed
       },
       formatters = {
         clang_format_18 = {
           command = "clang-format-18",
-          args = { "$FILENAME" },  -- optional args
-        }
+        },
       },
-      format_on_save = true,  -- optional: automatically format on save
+      format_on_save = true, -- optional: automatically format on save
     })
   end,
 }
