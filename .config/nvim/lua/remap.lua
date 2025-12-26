161d4198bef51bd13038f8af2370327b848622f3
Iver <iver@Iver-sin-MacBook-Pro.local>
2025-12-26 17:11:19 +0100

    Fixed remaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,10 +1,10 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
 vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
 vim.keymap.set('n', '<space>x', ':.lua<CR>')
 vim.keymap.set('v', '<space>x', ':lua<CR>')
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
 
 -- Indent while remaining in visual mode.
 vim.keymap.set('v', '<', '<gv')
