784724fa1a1b21dcdcc133e2a23f7ac7d536ba8d
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2026-02-28 15:51:40 +0000

    Added arch linux repos

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+
+--- Font config
+config.font_size = 14
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.enable_wayland = false
+
+--- Colors
+config.colors = {
+  cursor_bg = "white",
+  cursor_border = "white"
+}
+
+-- Apperance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+--- Misc settings
+config.max_fps = 120
+config.prefer_egl = true
+
+return config 
