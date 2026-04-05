b28ebaa4511bbdf1a91ba4249abce6e5dec863fa
iverfix <iver.andreas.ugelvik@hotmail.com>
2026-04-05 15:58:23 +0100

    Updated telescope and treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,9 @@
--- Install Lazy.
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require('autocmds')
 require('remap')
 require('set')
+require('plugins')
+require('plugins.telescope')
+require('plugins.treesitter')
+
 
 vim.opt.clipboard:append('unnamedplus')
-
--- Setup lazy.nvim
-require('lazy').setup({
-  spec = 'plugins',
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { 'habamax' } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,23 @@
-return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    build = ':TSUpdate', -- keeps parsers up to date
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'javascript',
-        'typescript',
-        'lua',
-        'rust',
-        'vim',
-        'vimdoc',
-      },
-      sync_install = false, -- install asynchronously
-      auto_install = true, -- install missing parsers on buffer open
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      -- you can add other modules here (e.g., incremental_selection, textobjects)
-    },
+require("nvim-treesitter").setup( {
+  ensure_installed = {
+    'bash',
+    'c',
+    'cpp',
+    'javascript',
+    'typescript',
+    'lua',
+    'rust',
+    'vim',
+    'vimdoc',
   },
-}
+  sync_install = false, -- install asynchronously
+  auto_install = true, -- install missing parsers on buffer open
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
+  -- you can add other modules here (e.g., incremental_selection, textobjects)
+})
