0a8d00c64f999311c745f9daa5d2aaa77efd6044
iverfix <iver.andreas.ugelvik@hotmail.com>
2026-04-05 16:04:36 +0100

    Migreated colors and gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@ require('set')
 require('plugins')
 require('plugins.telescope')
 require('plugins.treesitter')
+require('plugins.colors')
+require('plugins.gitsigns')
 
 
 vim.opt.clipboard:append('unnamedplus')
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,4 +3,6 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/catppuccin/nvim" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
